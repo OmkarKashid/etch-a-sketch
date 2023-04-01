5a73ab722b368b03ae80b1d18a3dc7d673249304
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-04-02 01:14:43 +0530

    Add JS functions for change color when hovor

--- script.js
+++ script.js
@@ -8,16 +8,30 @@ function createRows(totalRows){
 
 function createSquares(totalSquaresRow){
     const rows = document.querySelectorAll(".div-row");
+    let squareNo = 1 ;
     rows.forEach(row => {
         let squaresHtml = "";
         for(let i = 1; i <= totalSquaresRow; i++){
-            squaresHtml += `<div id="square${i}" class="div-square"></div>`;
+            squaresHtml += `<div id="square${squareNo}" class="div-square"></div>`;
+            squareNo++;
         }
         row.innerHTML =  squaresHtml;
     });
 }
 
+function changeSquareColor(event){
+    const squareId = event.target.id;
+    const square = document.querySelector(`#${squareId}`);
+    square.classList.add("hover");
+
+}
 const totalRows = 16;
 const totalSquaresRow = 16;
 createRows(totalRows);
 createSquares(totalSquaresRow);
+const squares = document.querySelectorAll(".div-square");
+const squaresArray = [...squares];
+squaresArray.forEach(square => {
+    square.addEventListener("mouseover", (event) => changeSquareColor(event));
+});
+console.log("end");
