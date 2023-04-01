512dca7e39006c58f553398ae0d8d6829f14a73e
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-04-02 01:25:14 +0530

    add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Function to create div rows */
 function createRows(totalRows){
     let rowsHtml = "";
     for(let i = 1; i <= totalRows; i++){
@@ -6,9 +7,10 @@ function createRows(totalRows){
     document.querySelector("#grid").innerHTML = rowsHtml;
 }
 
+/* Function to create individual square divs */
 function createSquares(totalSquaresRow){
     const rows = document.querySelectorAll(".div-row");
-    let squareNo = 1 ;
+    let squareNo = 1 ;    //increasing counter for individual square id
     rows.forEach(row => {
         let squaresHtml = "";
         for(let i = 1; i <= totalSquaresRow; i++){
@@ -19,19 +21,20 @@ function createSquares(totalSquaresRow){
     });
 }
 
+/* Function for adding hover class to single square div */
 function changeSquareColor(event){
     const squareId = event.target.id;
     const square = document.querySelector(`#${squareId}`);
     square.classList.add("hover");
 
 }
-const totalRows = 16;
-const totalSquaresRow = 16;
+const totalRows = 16; //total div rows to be created
+const totalSquaresRow = 16; //total squares per row to be created
 createRows(totalRows);
 createSquares(totalSquaresRow);
 const squares = document.querySelectorAll(".div-square");
 const squaresArray = [...squares];
 squaresArray.forEach(square => {
-    square.addEventListener("mouseover", (event) => changeSquareColor(event));
+    square.addEventListener("mouseover", (event) => changeSquareColor(event)); //add function when muse hovers
 });
-console.log("end");
+
