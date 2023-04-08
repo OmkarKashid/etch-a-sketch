1cf77dbb60120d9b264ad1149bda1415b03ea2c4
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-04-08 20:41:30 +0530

    add change grid button and functionality

--- script.js
+++ script.js
@@ -28,13 +28,37 @@ function changeSquareColor(event){
     square.classList.add("hover");
 
 }
+function removeSquareColor(event){
+    const squareId = event.target.id;
+    const square = document.querySelector(`#${squareId}`);
+    square.classList.remove("hover");
+}
+/* Function to resize Grid */
+function resizeGrid(){
+    let newRows = parseInt(prompt("Please write number of rows (Max 70)","16"));
+    let newSquaresRow = parseInt(prompt("Please write number of squares per row (Max 70)", "16"));
+    if(!Number.isInteger(newRows) || !Number.isInteger(newSquaresRow)) return ;
+    if((newRows > 70) || (newSquaresRow >70)){
+        newRows = 70;
+        newSquaresRow = 70;
+    }
+    createGrid(newRows, newSquaresRow);
+}
+/* Function to create a new grid */
+function createGrid(totalRows, totalSquaresRow){
+    createRows(totalRows);
+    createSquares(totalSquaresRow);
+    const squares = document.querySelectorAll(".div-square");
+    const squaresArray = [...squares];
+    squaresArray.forEach(square => {
+        square.addEventListener("mouseover", (event) => changeSquareColor(event)); //add function when muse hovers
+    });
+}
 const totalRows = 16; //total div rows to be created
 const totalSquaresRow = 16; //total squares per row to be created
-createRows(totalRows);
-createSquares(totalSquaresRow);
-const squares = document.querySelectorAll(".div-square");
-const squaresArray = [...squares];
-squaresArray.forEach(square => {
-    square.addEventListener("mouseover", (event) => changeSquareColor(event)); //add function when muse hovers
-});
+createGrid(totalRows, totalSquaresRow);
+const btnResize = document.querySelector("#btn_resize");
+btnResize.addEventListener("click", e => resizeGrid());
+const btnErase = document.querySelector("#btn_erase");
+btnErase.addEventListener("click", e => removeSquareColor(e));
 
